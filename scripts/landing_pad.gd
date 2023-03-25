1e6cc767262019a81d95e24e053c7f210a40140e
John Hannagan <weetbix_@hotmail.com>
2023-03-25 23:40:48 +1100

    Add difficulty text under pads

--- scripts/landing_pad.gd
+++ scripts/landing_pad.gd
@@ -3,17 +3,14 @@ class_name LandingPad
 
 @onready var landingTimer: Timer = $LandingTimer
 @onready var collisionShape: CollisionShape2D = $CollisionShape2D
+@onready var difficultyText: RichTextLabel = $DifficultyText
 
 var difficulty : float
 signal did_land(difficulty)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	difficultyText.text = "x%.1f" % difficulty
 
 func _on_area_entered(body:Node2D):
 	landingTimer.start()
